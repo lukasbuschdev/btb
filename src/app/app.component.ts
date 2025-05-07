436664d7f991a8de230116994a31a1e11f532108
lukasbuschdev <buschlukas175@gmail.com>
2025-05-06 23:10:28 -0600

    initial commit of btb (Back To Basics) page, implemented the following: variables.scss, basic components structure (header, nav, main, footer, dashboard, learn, practice, challenge), font ('Exo 2'), routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, MainComponent, FooterComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './main/dashboard/dashboard.component';
+import { LearnComponent } from './main/learn/learn.component';
+import { PracticeComponent } from './main/practice/practice.component';
+import { ChallengeComponent } from './main/challenge/challenge.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'learn', component: LearnComponent },
+    { path: 'practice', component: PracticeComponent },
+    { path: 'challenge', component: ChallengeComponent },
+];
